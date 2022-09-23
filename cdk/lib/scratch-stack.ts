d50c163a7f60f730c516d8a8597f54daa90c08c1
Robert Friberg <robert@devrex.se>
2022-09-23 21:43:22 +0200

    increase lambda memory

--- cdk/lib/scratch-stack.ts
+++ cdk/lib/scratch-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { RemovalPolicy } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
@@ -24,6 +24,8 @@ export class ScratchStack extends cdk.Stack {
         runtime: Runtime.NODEJS_16_X,
         handler: 'lambda.handler',
         code: Code.fromAsset("../vanilla-express"),
+        memorySize: 512,
+        timeout: Duration.seconds(10),
         environment: {
           'TABLE_NAME' : table.tableName
         }
