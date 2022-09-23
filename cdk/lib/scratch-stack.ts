de4981f01ae2561d33607fed347e788086007aa8
Robert Friberg <robert@devrex.se>
2022-09-23 18:35:56 +0200

    log event in lambda

--- cdk/lib/scratch-stack.ts
+++ cdk/lib/scratch-stack.ts
@@ -1,25 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import { RemovalPolicy, SecretValue } from 'aws-cdk-lib';
-import { RestApi } from 'aws-cdk-lib/aws-apigateway';
-import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
-import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { ManagedPolicy, Policy, User } from 'aws-cdk-lib/aws-iam';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 
 export class ScratchStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
-      const lambda = new Function(this, "lambda",
-      {
-        runtime: Runtime.NODEJS_16_X,
-        handler: 'lamba.handler',
-        code: Code.fromAsset("../vanilla-express")
-      })
 
       const table = new Table(this,'dynamo-table',{
         billingMode: BillingMode.PAY_PER_REQUEST,
@@ -29,10 +18,21 @@ export class ScratchStack extends cdk.Stack {
           type: AttributeType.STRING
         },
       })
+
+      const lambda = new Function(this, "lambda",
+      {
+        runtime: Runtime.NODEJS_16_X,
+        handler: 'lambda.handler',
+        code: Code.fromAsset("../vanilla-express"),
+        environment: {
+          'TABLE_NAME' : table.tableName
+        }
+      })
+
       table.grantReadWriteData(lambda)
 
-      const api = new RestApi(this, 'api', {
-        
+      new LambdaRestApi(this, 'api', {
+        handler: lambda
       })
    }
 }
--- vanilla-express/lambda.js
+++ vanilla-express/lambda.js
@@ -3,4 +3,7 @@ const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
 
-exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
+exports.handler = (event, context) => { 
+    console.log(event)
+    awsServerlessExpress.proxy(server, event, context)
+}
